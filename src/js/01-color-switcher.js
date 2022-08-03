be9f71a078b559c329763e0fde2a94a8bc946b76
Анастасия Маслак <96750008+AnastasiiaKim1996@users.noreply.github.com>
2022-08-04 01:16:22 +0300

    add task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,63 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+const btn = document.querySelector('button');
+
+let timeID = null;
+
+let isActive = false;
+
+startBtn.addEventListener('click', onStartSwitchColor);
+stopBtn.addEventListener('click', onStopSwitchColor);
+
+function onStartSwitchColor() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  if (
+    (timeID = setInterval(() => {
+      bodyRef.style.background = getRandomHexColor();
+      console.log(`Switch color`, timeID);
+    }, 1000))
+  ) {
+    startBtn.toggleAttribute('disabled');
+  }
+}
+
+function onStopSwitchColor() {
+  clearInterval(timeID);
+  console.log(`Stop switch color`, timeID);
+  isActive = false;
+  if (startBtn.hasAttribute('disabled')) {
+    startBtn.removeAttribute('disabled');
+  }
+}
+
+// --- second solution --- //
+
+// const changeBgColor = el => (el.style.backgroundColor = getRandomHexColor());
+// const toggleDisabled = (...elems) =>
+//   elems.forEach(el => el.toggleAttribute('disabled'));
+
+// const startSwichColor = btn => {
+//   if (btn === startBtn) {
+//     changeBgColor(bodyRef);
+//     timeID = setInterval(() => changeBgColor(bodyRef), 1000);
+//   }
+//   if (btn === stopBtn) clearInterval(timeID);
+// };
+
+// const onBtnClick = ({ target }) => {
+//   if (!btn || btn.hasAttribute('disabled')) return;
+
+//   toggleDisabled(startBtn, stopBtn);
+//   startSwichColor(btn);
+// };
+
+// bodyRef.addEventListener('click', onBtnClick);
---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+// - import - //
+
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// --- Timer --- //
+
+const inputRef = document.querySelector('#datetime-picker');
+const startBtnRef = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+
+const options = { once: true, passive: true };
+
+let userChooseDate = null;
+
+const setDisabled = (...elems) => {
+  elems.forEach(el => el.setAttribute('disabled', true));
+};
+
+const setEnabled = (...elems) => {
+  elems.forEach(el => el.removeAttribute('disabled'));
+};
+
+flatpickr(inputRef, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      Notify.failure('Please choose a date in the future');
+    } else {
+      userChooseDate = selectedDates[0];
+      setEnabled(startBtnRef);
+    }
+  },
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function updateUserInterface({ days, hours, minutes, seconds }) {
+  daysRef.textContent = addLeadingZero(days);
+  hoursRef.textContent = addLeadingZero(hours);
+  minutesRef.textContent = addLeadingZero(minutes);
+  secondsRef.textContent = addLeadingZero(seconds);
+}
+
+function onStartButtonClick() {
+  setDisabled(inputRef, startBtnRef);
+
+  const timerId = setInterval(() => {
+    let leftoverTime = userSelectedDate - new Date();
+
+    if (leftoverTime <= 0) {
+      clearInterval(timerId);
+      leftoverTime = 0;
+    }
+
+    const resultTime = convertMs(leftoverTime);
+    updateUserInterface(resultTime);
+  }, 1000);
+}
+
+startBtnRef.addEventListener('click', onStartButtonClick, options);
